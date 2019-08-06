45e9f4f75ed6c644b5c72c805af2a8ac92918c7e
krzaq <krzaq@krzaq.cc>
2019-08-06 12:14:03 +0200

    Change the folder structure.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 #define DEBUG(x) std::cout << __FILE__ << ":" << __LINE__ << ":" <<\
     #x << ":" << x << std::endl;
 
-#include "proto.hpp"
+#include "protocol_parser_generator/proto.hpp"
 
 namespace kq
 {
-using mcr = message<
+using mov = message<
     element<struct begin, char_constant<'['>>,
     element<struct message_type, text<8>>,
     element<struct x_to, number<3>>,
@@ -20,6 +20,15 @@ using mcr = message<
 
 int main()
 {
-    // DEBUG(f2.value<name>());
-    // DEBUG(f2.value<age>());
+    kq::mov m;
+    m.value<kq::message_type>() = "MOV";
+    m.value<kq::x_to>() = 13;
+    m.value<kq::y_to>() = 37;
+    DEBUG(m.to_string());
+
+    kq::mov m2 = kq::mov::parse(m.to_string);
+
+    DEBUG(m2.value<kq::message_type>());
+    DEBUG(m2.value<kq::x_to>());
+    DEBUG(m2.value<kq::y_to>());
 }
